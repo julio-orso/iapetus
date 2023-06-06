ef1f6fad9b10d6a4bad2a36a7da43ced2b9038c6
julio3g <juliocesar.orso@gmail.com>
2023-06-06 16:21:15 -0300

    feat: 🎉 add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,55 +1,17 @@
-import '../styles/globals.scss'
+import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
-import { Header } from '../components/Header'
-import { Footer } from '../components/Footer'
 import { Toaster } from 'react-hot-toast'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import * as gtag from '../lib/gtag'
-import Script from 'next/script'
-
-// <!-- Global site tag (gtag.js) - Google Analytics -->
-// <script async src="https://www.googletagmanager.com/gtag/js?id=UA-158569684-3"></script>
-// <script>
-//   window.dataLayer = window.dataLayer || [];
-//   function gtag(){dataLayer.push(arguments);}
-//   gtag('js', new Date());
-
-//   gtag('config', 'UA-158569684-3');
-// </script>
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+import '../styles/globals.scss'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url: any) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    router.events.on('hashChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-      router.events.off('hashChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=UA-158569684-3`}
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-       window.dataLayer = window.dataLayer || [];
-       function gtag(){dataLayer.push(arguments);}
-       gtag('js', new Date());
-
-       gtag('config', 'UA-158569684-3');
-       `}
-      </Script>
       <Header />
       <Toaster position="bottom-right" />
       <Component {...pageProps} />
+      <Analytics />
       <Footer />
     </>
   )
